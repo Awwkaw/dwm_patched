82034ddf71a744738e3e4e093c189006be5be7a0
Tekc <tekc@chem.au.dk>
2019-09-12 12:32:02 +0200

    made a working dwm patched with alpha

--- config.mk
+++ config.mk
@@ -18,8 +18,8 @@ FREETYPEINC = /usr/include/freetype2
 #FREETYPEINC = ${X11INC}/freetype2
 
 # includes and libs
-INCS = -I${X11INC} -I${FREETYPEINC}
-LIBS = -${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lXrender
+INCS = -I${X11INC} -I${FREETYPEINC} -I/usr/include/freetype2 -I/usr/include/libpng16
+LIBS = ${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lXrender
 
 # flags
 CPPFLAGS += -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
